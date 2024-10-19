9f28ee7e16f08a8df2c000d767f0f38a8444b7ff
josiahtripp <josiahtripp@gmail.com>
2024-10-19 10:06:02 -0500

    Completed argument parsing

--- cebc.c
+++ cebc.c
@@ -7,9 +7,25 @@ enum alloc_type { // Determines block allocation in c-file
 };
 
 char* input_filename = NULL;
-char* output_filename = NULL; // Filename will be set to input-file name with no extension
-int blocks_amount = 30000; // As per standard Brainf*ck, limit of 30,000 blocks
+char* output_filename = "output.c"; // Filename will be set to input-file name with no extension
+int block_amount = 30000; // As per standard Brainf*ck, limit of 30,000 blocks
 enum alloc_type memory_type = STATIC; // Default to static memory
+FILE* input_file;
+FILE* output_file;
+
+char** c_file_template_static = {
+
+    "#include <stdio.h>\n",
+    "#inlcude <stdlib.h>\n"
+    "register int pointer = 0;\n",
+    "static __uint8_t* blocks\n",
+    "int main(){\n",
+    "for(register int i = 0; i < ",
+    NULL,// Add block amount
+    "; i++) blocks[i] = 0;\n",
+    NULL,
+
+};
 
 int main(int argc, char** argv){
 
@@ -27,7 +43,15 @@ int main(int argc, char** argv){
                 break;
 
                 case 'b':// Set custom block amount
-                    blocks_amount = atoi(argv[++i]);
+                    block_amount = atoi(argv[++i]);
+                break;
+
+                case 's':// Set memory type to static
+                    memory_type = STATIC;
+                break;
+
+                case 'd':// Set memory type to dynamic
+                    memory_type = DYNAMIC;
                 break;
 
                 default:
@@ -42,6 +66,80 @@ int main(int argc, char** argv){
         }
     }
 
+    // Open the Brainf*ck file
+    input_file = fopen(input_filename, "r");
 
+    // Check file status
+    if(!input_file){
+        printf("cebc: Error: input file '%s' could not be found", input_filename);
+        return 1;
+    }
+
+    // Open the c-output file
+    output_file = fopen(output_filename, "w");
+
+    int count = 0;
+    char* strptr = c_file_template_static[count];
+
+    while(strptr){
+        fprintf(output_file, "%s", strptr);
+        count++;
+        strptr = c_file_template_static[count];
+    }
+
+    fprintf(output_file, "%d", block_amount);
+    count++;
+    strptr = c_file_template_static[count];
+
+    while(strptr){
+        fprintf(output_file, "%s", strptr);
+        count++;
+        strptr = c_file_template_static[count];
+    }
+
+    // Read all characters in the file
+    char tmp;
+    while((tmp = getc(input_file))){
+
+        switch (tmp){
+
+            case '<':
+                fprintf(output_file, "pointer--;\n");
+            break;
+
+            case '>':
+                fprintf(output_file, "pointer++;\n");
+            break;
+
+            case '-':
+                fprintf(output_file, "blocks[pointer]--;\n");
+            break;
+
+            case '+':
+                fprintf(output_file, "blocks[pointer]++;\n");
+            break;
+
+            case '.':
+                fprintf(output_file, "blocks[pointer] = getchar();\n");
+            break;
+
+            case ',':
+                fprintf(output_file, "putchar(blocks[pointer]);\n");
+            break;
+
+            case '[':
+                fprintf(output_file, "while(blocks[pointer]){\n");
+            break;
+
+            case ']':
+                fprintf(output_file, "}\n");
+            break;
+
+        }
+    }
+
+    fprintf(output_file, "return 0;}");
+    fclose(output_file);
+    fclose(input_file);
     return 0;
 }
